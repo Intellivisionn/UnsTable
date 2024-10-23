a1d5a5001c9d4514064f54f12991b15fa4c4ff3b
martpec <95352759+martpec@users.noreply.github.com>
2024-10-23 15:30:45 +0200

    connecting database

--- unstable/lib/main.dart
+++ unstable/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:unstable/pages/home_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: "https://gchpzrrdcleabasvhqhv.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjaHB6cnJkY2xlYWJhc3ZocWh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2ODU4NTUsImV4cCI6MjA0NTI2MTg1NX0.3vz9fmw-pkaDpLdpP67ohwbYuNg00hCfEbt-NlGVBvk",
+  );
   runApp(const MyApp());
 }
 
